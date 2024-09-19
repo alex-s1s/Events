9f2efe34ea2a35a83892b8bd208857490a66d712
alex mendes <42866851+alex-s1s@users.noreply.github.com>
2024-09-19 00:18:41 -0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,1 @@
-import firebase from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "token",
-  authDomain: "crm-scrap.firebaseapp.com",
-  projectId: "crm-scrap",
-  storageBucket: "crm-scrap.appspot.com",
-  messagingSenderId: "758253671500",
-  appId: "appId",
-};
-
-export default firebase.initializeApp(firebaseConfig);
